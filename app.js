68e053931ac6e3c2c58b942fe909fab9ea3f1ada
dimitris-thomopoulos <thomopoulos59@gmail.com>
2022-09-20 16:16:15 +0300

    2nd commit - Completed header

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
-$(document).ready(function() {
-    // DOCUMENT READY START
-    
-    // MOBILE MENU
-    function toggle_mobile_menu() {
-        $("#mobile-menu-items").stop(true, true).fadeToggle(200);
-        $("#mobile-menu-overlay").stop(true, true).fadeToggle(200);
-    }
-    $(".mobile-menu-icon").click(function() {
-        toggle_mobile_menu();
-    });
-    $("#mobile-menu-overlay").click(function() {
-        toggle_mobile_menu();
-    });
-    
-    // SMOOTHSCROLL
-    $("a.scroller").smoothScroll({
-        //afterScroll: function() {location.hash = this.hash;}
-    });
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 160) {
-            $('#scroll-top').fadeIn();
-            $('#main').addClass('scrolled');
-        } else {
-            $('#scroll-top').fadeOut();
-            $('#main').removeClass('scrolled');
-        }
-    });
-    
-});
+// Burger Menu
+
+var burgerMenu = document.querySelector('#burger-menu');
+var line1 = document.querySelector('#burger-menu .line:nth-of-type(1)');
+var line2 = document.querySelector('#burger-menu .line:nth-of-type(2)');
+var line3 = document.querySelector('#burger-menu .line:nth-of-type(3)');
+
+
+burgerMenu.addEventListener('click', () => {
+
+    line1.classList.toggle('inactive');
+    line1.classList.toggle('active');
+
+    line2.classList.toggle('inactive');
+    line2.classList.toggle('active');
+
+    line3.classList.toggle('inactive');
+    line3.classList.toggle('active');
+
+})
+
+
+// Sticky Navbar
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// get the navbar links
+
+var navbarLinks = [...document.querySelectorAll('nav ul li a')];
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= 71.99) {
+    navbar.classList.add("sticky");
+    navbar.classList.remove("position-absolute");
+
+    // for (let i=0; i<navbarLinks.length; i++) {
+    //     navbarLinks[i].classList.add("sticky");
+    // };
+
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.classList.add("position-absolute");
+
+    // for (let i=0; i<navbarLinks.length; i++) {
+    //     navbarLinks[i].classList.remove("sticky");
+    // };
+  }
+}
